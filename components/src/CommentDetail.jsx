f3af9dda58f394aa55c4cd832b9046e374546916
Elias Norrby <elias.norrby@gmail.com>
2019-08-16 08:34:15 +0200

    L35: Use props for everything in Comment

--- components/src/CommentDetail.jsx
+++ components/src/CommentDetail.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import faker from "faker";
 
 const CommentDetail = props => {
-  const { author } = props;
+  const { author, timeAgo, commentText, avatar } = props;
 
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={avatar} />
       </a>
       <div className="content">
         <a className="author" href="/">
           {author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{commentText}</div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        commentText="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        commentText="I agree!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 5:00PM"
+        commentText="You're awesome!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
