70c0517c0d748f383088a6bd207ffce90dd52131
Elias Norrby <elias.norrby@gmail.com>
2019-08-16 12:17:25 +0200

    L65

--- seasons/src/SeasonDisplay.jsx
+++ seasons/src/SeasonDisplay.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = ({ lat }) => {
-  return <div>Season Display: {lat}</div>;
+  const season = getSeason(lat, new Date().getMonth());
+  const text =
+    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
