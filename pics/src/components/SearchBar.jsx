c984bda58b9c34711f428cec245962cda1343432
Elias Norrby <elias.norrby@gmail.com>
2019-08-16 14:29:26 +0200

    L83: Handling Form Submittal

--- pics/src/components/SearchBar.jsx
+++ pics/src/components/SearchBar.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label htmlFor="search">Image Search</label>
             <input
