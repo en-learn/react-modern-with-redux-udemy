43c7eda6c6c285f1d82601171085ba98eb4c6a43
Elias Norrby <elias.norrby@gmail.com>
2019-08-19 18:32:51 +0200

    L90: Viewing Request Results
    I did a little workaround to avoid checking in the API key. There should
    be a refactor coming in a future lecture.

--- pics/src/components/App.jsx
+++ pics/src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
+import settings from '../config.local';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: `Client-ID ${settings.unsplashApiKey}`,
+      },
+    });
   }
 
   render() {
